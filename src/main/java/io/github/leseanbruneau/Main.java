45685e5c28d8f0275f7bc9e92a88c6c97226464a
ustleseanbruneau <ustbrun9912@gmail.com>
2023-03-29 08:49:46 -0500

    Initial commit

---
+++ src/main/java/io/github/leseanbruneau/Main.java
@@ -0,0 +1,14 @@
+package io.github.leseanbruneau;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class.getSimpleName());
+    public static void main(String[] args) {
+
+        log.info("Test Gradle project using IDEA IntelliJ IDE....");
+
+    }
+}
